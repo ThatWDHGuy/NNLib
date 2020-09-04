5b2eeee5e298a5056a2b671a25c4ad67a5fb9220
William <Will@oceanplait.com>
2020-09-05 00:17:59 +1200

    YEAH! prints! also pushing every single change

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 
 int main(void){
     NNLib nn = NNLib();
-    std::vector<uint8_t> i{16,32,64,128,8}; 
+    std::vector<uint8_t> i{16,32,64,128,8};
+    std::cout<<"Starting net creation"<<std::endl;
     nn.setLayers(&i);
+    std::cout<<"Layers made"<<std::endl;
     nn.makeLinks(nn.ALL);
+    std::cout<<"Links made"<<std::endl;
     nn.randWeightBias();
+    std::cout<<"Weights and bias set"<<std::endl;
     return 0;
 }
